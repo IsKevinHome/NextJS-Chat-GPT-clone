135c6d63f499183239136dfd9b494acd9c0ee4fb
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2023-12-23 18:29:16 -0800

    feat: :sparkles: Create left navigation, add icons, add logout

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -1,10 +1,15 @@
 "use client";
 
 import React from "react";
-import { useUser, UserProvider } from "@auth0/nextjs-auth0/client";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import LeftSection from "../components/LeftSection";
+import RightSection from "../components/RightSection";
 
 export default function Home() {
   const { user, isLoading } = useUser();
 
-  return <>chat page</>;
+  return <>
+  <LeftSection />
+  <RightSection />
+  </>;
 }
