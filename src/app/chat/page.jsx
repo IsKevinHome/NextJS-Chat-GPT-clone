3f63c21a594d92d2859aab4cf943622925c22746
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2023-12-24 22:35:28 -0800

    add textfield and send button

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -1,11 +1,31 @@
 'use client';
 
-export default function Home() {
+import ChatSidebar from '../components/ChatSidebar';
+
+export default function ChatPage() {
   return (
     <>
-      <div class='grid grid-cols-[260px_auto] h-screen'>
-        <div className="bg-neutral-900">side bar</div>
-        <div className='bg-neutral-500'>chat window</div>
+      <div className='grid grid-cols-[260px_auto] h-screen'>
+        <ChatSidebar />
+        <div className='bg-neutral-800 flex flex-col'>
+          <div className='flex-1'>Chat Window</div>
+          <footer className='bg-neutral-800 p-10'>
+            <form>
+              <fieldset className='flex gap-2'>
+                <textarea
+                  className=' outline-1 w-full resize-none rounded-md bg-neutral-700 p-2 text-white focus:border-blue-500 focus:bg-neutral-600 focus:outline focus:outline-blue-500'
+                  placeholder='Send a message...'
+                ></textarea>
+                <button
+                  type='submit'
+                  className='rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600'
+                >
+                  Send
+                </button>
+              </fieldset>
+            </form>
+          </footer>
+        </div>
       </div>
     </>
   );
---
+++ src/app/components/ChatSidebar.jsx
@@ -0,0 +1,9 @@
+const ChatSidebar = () => {
+    return (
+      <div className="bg-neutral-900 text-white">
+        <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  };
+  
+  export default ChatSidebar;
