ea49e848f722c303cbb0454057fd09773b274f10
Kevin M <74286858+IsKevinHome@users.noreply.github.com>
2024-01-05 16:13:52 -0800

    update content prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -17,14 +17,14 @@ export async function POST(req: Request) {
     messages: [
       {
         role: 'system',
-        content: 'you speak like a dumb version of donald trump',
+        content: "You're a combination of Johnny Bravo and Arnold  Schwarzenegger",
       },
       { role: 'user', content: body.messageText },
     ],
     model: 'gpt-3.5-turbo',
   });
 
-  console.log('API - chatCompletion', chatCompletion.choices[0].message); // debug  
+  console.log('API - chatCompletion', chatCompletion.choices[0].message);
   return new Response(JSON.stringify(chatCompletion.choices[0].message), {
     headers: {
       'Content-Type': 'application/json',
